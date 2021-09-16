6a026336d6028bb864882efa8f728419170cf186
ritikjain16 <ritik9628@gmail.com>
2021-09-16 07:24:46 +0530

    1 commit

---
+++ src/components/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <div className="container d-flex align-items-center mt-3">
+            <button className="btn btn-primary">-</button>
+            <span className="mx-2">Add To Cart</span>
+            <button className="btn btn-primary">+</button>
+        </div>
+    )
+}
+
+export default Shop
